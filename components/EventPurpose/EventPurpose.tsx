18ef11b1ce3c7e6e02b1ac019e919887a18cd5b0
aikoikom <aikoiko.m@gmail.com>
2025-10-27 12:03:50 +0900

    Update EventPurpose.tsx

--- components/EventPurpose/EventPurpose.tsx
+++ components/EventPurpose/EventPurpose.tsx
@@ -20,11 +20,7 @@ export default function EventPurpose() {
             シールラリーなどの企画も考えているため、SNS（Twitter/現X）を要チェック！
           </p>
 <br></br>
-<a href="https://twitter.com/x?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">
-  Follow @x
-</a>
-          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8">
-          </script>
+<a class="twitter-timeline" href="https://twitter.com/gbc_shinoosaka?ref_src=twsrc%5Etfw">Tweets by gbc_shinoosaka</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
           
         </div>
       </div>
