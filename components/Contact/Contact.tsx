7751690a9c538ac560762f23b91d542b3ee97c29
aikoikom <aikoiko.m@gmail.com>
2025-10-27 12:02:48 +0900

    Update Contact.tsx

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -13,8 +13,8 @@ export default function Contact() {
           <div className={styles.contactInfo}>
             <p className={styles.organizer}>主催者：ico</p>
             <p className={styles.email}>
-              MAIL: aikoiko.m@gmail.com</p>
-            <br></br>
+              MAIL: aikoiko.m@gmail.com<br></br></p>
+            
            <p className={styles.contactbutton}>
              <a href="https://example.com/contact" target="_blank">
              お問い合わせフォーム</a></p>
