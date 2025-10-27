f4ce67b8b5e4ba1eec8ec4a15ba0b6ea44a82510
aikoikom <aikoiko.m@gmail.com>
2025-10-27 13:03:44 +0900

    Update CircleParticipation.tsx

--- components/CircleParticipation/CircleParticipation.tsx
+++ components/CircleParticipation/CircleParticipation.tsx
@@ -7,7 +7,7 @@ export default function CircleParticipation() {
         <h2 className={styles.heading}>サークル参加方法</h2>
         <div className={styles.content}>
           <div className={styles.step}>
-            <h3 className={styles.stepTitle}>1. こみっくトレジャー47でサークル参加申込</h3>
+            <h3 className={styles.stepTitle}>1. <a href="https://www.aoboo.jp/event/item/p0087.html">こみっくトレジャー47</a>へサークル参加申込</h3>
             <ul className={styles.stepList}>
               <li>ジャンルコード: W001_パロディ：アニメ原作_こみっく★トレジャー</li>
               <li>備考欄に「『新大阪（仮）』連結希望」と記載すること</li>
@@ -15,7 +15,12 @@ export default function CircleParticipation() {
           </div>
           <div className={styles.step}>
             <h3 className={styles.stepTitle}>2. 参加申請フォーム送信</h3>
-            <p>お申し込み完了後に「このフォーム」にて参加申請をお願いします。</p>
+            <p>お申し込み完了後に下記フォームにて参加申請をお願いします。</p>
+                      <div className={styles.buttonarea}>
+           <p className={styles.button}>
+             <a href="https://docs.google.com/forms/d/e/1FAIpQLSfZurn-tWdMHxN5c61e96gXfx313cVc_V1d1JGKy9X-mr1nMA/viewform?usp=header" target="_blank">
+             「新大阪（仮）」お申込みフォーム</a></p>
+         </div>
           </div>
           <div className={styles.step}>
             <h3 className={styles.stepTitle}>3. 当日の頒布</h3>
