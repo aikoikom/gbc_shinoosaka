5d01e03b2bbf37c793a6f17824a9c5629aac119d
aikoikom <aikoiko.m@gmail.com>
2025-10-27 13:28:39 +0900

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p className={styles.copyright}>
-          2025 ガールズバンドクライプチオンリー 新大阪（仮）
+       ガールズバンドクライプチオンリー 新大阪（仮）
         </p>
       </div>
     </footer>
