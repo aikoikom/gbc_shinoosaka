886234a24f9aaffad106d2e2f5790bd8a1b4b1d2
aikoikom <aikoiko.m@gmail.com>
2025-11-11 13:11:53 +0900

    Update CircleList.tsx

--- components/CircleList/CircleList.tsx
+++ components/CircleList/CircleList.tsx
@@ -14,19 +14,100 @@ export default function CircleList() {
     <figure className={styles.circlethumb}>
       <img src="/gbc_shinoosaka/images/CC/02.jpeg" alt="新大阪（仮）本部のサークルカット" />
     </figure>
-
     <div className={styles.circlebody}>
       <h3 className={styles.circlename}>新大阪（仮）本部</h3>
       <span className={styles.circlehandle}>ｉｃｏ</span>
       <p className={styles.circleintro}>プチオンリー本部です。まずはこちらにお立ち寄りいただき、パンフレットをお受け取り下さい。</p>
-
-
       <div className={styles.circlesocial}>
         <a href="https://x.com/gbc_shinoosaka" className={styles.snslink}>Twitter / X</a>
       </div>
-    
     </div>
   </article>
+
+  <article className={styles.circlecard}>
+    <figure className={styles.circlethumb}>
+      <img src="/gbc_shinoosaka/images/CC/03.png" alt="トポロジカルブランケットのサークルカット" />
+    </figure>
+    <div className={styles.circlebody}>
+      <h3 className={styles.circlename}>トポロジカルブランケット</h3>
+      <span className={styles.circlehandle}>k5</span>
+      <p className={styles.circleintro}>仁菜ヒナ！仁菜ヒナ！仁菜ヒナ！仁菜ヒナ！</p>
+      <div className={styles.circlesocial}>
+        <a href="https://x.com/TiroDuetto" className={styles.snslink}>Twitter / X</a>
+      </div>
+    </div>
+  </article>
+
+    <article className={styles.circlecard}>
+    <figure className={styles.circlethumb}>
+      <img src="/gbc_shinoosaka/images/CC/04.png" alt="ぼたさんちのサークルカット" />
+    </figure>
+    <div className={styles.circlebody}>
+      <h3 className={styles.circlename}>ぼたさんち</h3>
+      <span className={styles.circlehandle}>甘味トロ</span>
+      <p className={styles.circleintro}>ヒナがトゲナシトゲアリに加入している世界に仁菜が異世界転生するお話です！</p>
+      <div className={styles.circlesocial}>
+        <a href="https://x.com/botatoro3" className={styles.snslink}>Twitter / X</a>
+      </div>
+    </div>
+  </article>
+
+    <article className={styles.circlecard}>
+    <figure className={styles.circlethumb}>
+      <img src="/gbc_shinoosaka/images/CC/05.png" alt="びびっどサンダーのサークルカット" />
+    </figure>
+    <div className={styles.circlebody}>
+      <h3 className={styles.circlename}>新大阪（仮）本部</h3>
+      <span className={styles.circlehandle}>ズミクニ</span>
+      <p className={styles.circleintro}>ガールズバンドクライの桃香さんが悩む本</p>
+      <div className={styles.circlesocial}>
+        <a href="https://x.com/zumikuni" className={styles.snslink}>Twitter / X</a>
+      </div>
+    </div>
+  </article>
+
+    <article className={styles.circlecard}>
+    <figure className={styles.circlethumb}>
+      <img src="/gbc_shinoosaka/images/CC/06.png" alt="まどぎわのひとのサークルカット" />
+    </figure>
+    <div className={styles.circlebody}>
+      <h3 className={styles.circlename}>まどぎわのひと</h3>
+      <span className={styles.circlehandle}>なるかぜ</span>
+      <p className={styles.circleintro}>ガールズバンドクライの日常作品などをまとめました。</p>
+      <div className={styles.circlesocial}>
+        <a href="https://x.com/narukaze__" className={styles.snslink}>Twitter / X</a>
+      </div>
+    </div>
+  </article>
+
+    <article className={styles.circlecard}>
+    <figure className={styles.circlethumb}>
+      <img src="/gbc_shinoosaka/images/CC/07.png" alt="あつあつフライヤーのサークルカット" />
+    </figure>
+    <div className={styles.circlebody}>
+      <h3 className={styles.circlename}>あつあつフライヤー</h3>
+      <span className={styles.circlehandle}>フライング・フライドポテト</span>
+      <p className={styles.circleintro}>ニナヒナの漫画です。グッズとかも考えてます。</p>
+      <div className={styles.circlesocial}>
+        <a href="https://x.com/flyingfriesfree" className={styles.snslink}>Twitter / X</a>
+      </div>
+    </div>
+  </article>
+
+    <article className={styles.circlecard}>
+    <figure className={styles.circlethumb}>
+      <img src="/gbc_shinoosaka/images/CC/08.png" alt="群青温泉湯あがり庵のサークルカット" />
+    </figure>
+    <div className={styles.circlebody}>
+      <h3 className={styles.circlename}>群青温泉湯あがり庵</h3>
+      <span className={styles.circlehandle}>群青温泉</span>
+      <p className={styles.circleintro}>初のガルクラSS本です！日常系がお好みの方にオススメしたいです♪よろしくお願いします！</p>
+      <div className={styles.circlesocial}>
+        <a href="https://x.com/gunjo_spa" className={styles.snslink}>Twitter / X</a>
+      </div>
+    </div>
+  </article>
+  
 </section>
 
 
