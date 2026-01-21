1552168540758c1c58721e1128725fcfb423f164
aikoikom <aikoiko.m@gmail.com>
2026-01-21 22:45:28 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'ガールズバンドクライプチオンリー　新大阪（仮）',
+  title: 'ガールズバンドクライプチオンリー',
   description: '2026/01/18(日)にインテックス大阪で開催される、「ガールズバンドクライ」のプチオンリーです。',
   keywords: ['ガールズバンドクライ', 'ガルクラ', 'プチオンリー', 'コミックトレジャー', '大阪'],
 }
